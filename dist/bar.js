399bca706937d304f4f767eebb6e969efbcef7e8
zengxiaoluan <370203400@qq.com>
2019-01-21 17:08:20 +0800

    add a demo with reduced code seajs

--- dist/bar.js
+++ dist/bar.js
@@ -1,5 +1,5 @@
-define('file:///D:/zengxiaoluan/learn_seajs/dist/bar.js', ['file:///D:/zengxiaoluan/learn_seajs/dist/bar2.js'], function(require, exports, module) {
-  var bar2 = require('file:///D:/zengxiaoluan/learn_seajs/dist/bar2.js');
+define('https://zengxiaoluan.github.io/learn_seajs/dist/bar.js', ['https://zengxiaoluan.github.io/learn_seajs/dist/bar2.js'], function(require, exports, module) {
+  var bar2 = require('https://zengxiaoluan.github.io/learn_seajs/dist/bar2.js');
 
   module.exports = {
     bar: function () {
--- dist/bar2.js
+++ dist/bar2.js
@@ -1,4 +1,4 @@
-define('file:///D:/zengxiaoluan/learn_seajs/dist/bar2.js', [], function(require, exports, module) {
+define('https://zengxiaoluan.github.io/learn_seajs/dist/bar2.js', [], function(require, exports, module) {
   
   module.exports = {
     bar2: function () {
